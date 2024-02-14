900a10cdc6dd176f4a54989b765cf43ee3b5db00
Ronit z <ronitz@webmobtech.com>
2024-02-14 18:16:08 +0530

    Update template file

--- resources/views/admin/role/show.blade.php
+++ resources/views/admin/role/show.blade.php
@@ -1,17 +1,17 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Roles') }}
         </h2>
     </x-slot>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="px-6 flex justify-between items-center">
-                    <h1 class="inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 py-4 block sm:inline-block flex">{{ __('View role') }}</h1>
-                    <a href="{{route('role.index')}}" class="px-4 py-2 text-white mr-4 bg-blue-600">{{ __('Back to all role') }}</a>
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="flex items-center justify-between px-6">
+                    <h1 class="flex block inline-block py-4 text-2xl font-extrabold tracking-tight sm:text-3xl text-slate-900 dark:text-slate-200 sm:inline-block">{{ __('View role') }}</h1>
+                    <a href="{{route('role.index')}}" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">{{ __('Back to all role') }}</a>
                     @if ($errors->any())
-                    <ul class="mt-3 list-none list-inside text-sm text-red-400">
+                    <ul class="mt-3 text-sm text-red-400 list-none list-inside">
                         @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
                         @endforeach
@@ -20,22 +20,22 @@
                 </div>
                 <div class="w-full px-6 py-4">
                     <div class="min-w-full border-b border-gray-200 shadow">
-                        <table class="table-fixed w-full text-sm">
+                        <table class="w-full text-sm table-fixed">
                             <tbody class="bg-white dark:bg-slate-800">
                                 <tr>
-                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ __('Name') }}</td>
-                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{{$role->name}}</td>
+                                    <td class="p-4 pl-8 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">{{ __('Name') }}</td>
+                                    <td class="p-4 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">{{$role->name}}</td>
                                 </tr>
                                 <tr>
                                     @unless ($role->name == env('APP_SUPER_ADMIN', 'super-admin'))
-                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ __('Permissions') }}</td>
-                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
+                                    <td class="p-4 pl-8 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">{{ __('Permissions') }}</td>
+                                    <td class="p-4 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">
                                         <div class="py-2">
                                             <div class="grid grid-cols-4 gap-4">
                                                 @forelse ($permissions as $permission)
                                                 <div class="col-span-4 sm:col-span-2 md:col-span-2">
                                                     <label class="form-check-label">
-                                                        <input type="checkbox" name="permissions[]" value="{{ $permission->name }}" {{ in_array($permission->id, $roleHasPermissions) ? 'checked' : '' }} disabled="disabled" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                                        <input type="checkbox" name="permissions[]" value="{{ $permission->name }}" {{ in_array($permission->id, $roleHasPermissions) ? 'checked' : '' }} disabled="disabled" class="text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                                         {{ $permission->name }}
                                                     </label>
                                                 </div>
@@ -48,8 +48,8 @@
                                 </tr>
                                 @endunless
                                 <tr>
-                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ __('Created') }}</td>
-                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">{{$role->created_at}}</td>
+                                    <td class="p-4 pl-8 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">{{ __('Created') }}</td>
+                                    <td class="p-4 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">{{$role->created_at}}</td>
                                 </tr>
                             </tbody>
                         </table>
--- resources/views/admin/user/profile.blade.php
+++ resources/views/admin/user/profile.blade.php
@@ -1,40 +1,40 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('My Account') }}
         </h2>
     </x-slot>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="px-6">
-                    <h1 class="inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 py-4 block sm:inline-block flex">{{ __('Account Info') }}</h1>
+                    <h1 class="flex block inline-block py-4 text-2xl font-extrabold tracking-tight sm:text-3xl text-slate-900 dark:text-slate-200 sm:inline-block">{{ __('Account Info') }}</h1>
                     @if ($errors->account->any())
-                    <ul class="mt-3 list-none list-inside text-sm text-red-400">
+                    <ul class="mt-3 text-sm text-red-400 list-none list-inside">
                         @foreach ($errors->account->all() as $error)
                         <li>{{ $error }}</li>
                         @endforeach
                     </ul>
                     @endif
                     @if(session()->has('account_message'))
-                    <div class="mb-8 text-green-400 font-bold">
+                    <div class="mb-8 font-bold text-green-400">
                         {{ session()->get('account_message') }}
                     </div>
                     @endif
                 </div>
-                <div class="w-full px-6 py-4 bg-white overflow-hidden">
+                <div class="w-full px-6 py-4 overflow-hidden bg-white">
                     <form method="POST" action="{{ route('admin.profile.info.store') }}">
                         @csrf
                         <div class="py-2">
-                            <label for="name" class="block font-medium text-sm text-gray-700{{$errors->account->has('name') ? ' text-red-400' : ''}}">{{ __('Name') }}</label>
+                            <label for="name" class="block text-sm font-medium text-gray-700">{{ __('Name') }}</label>
                             <input id="name" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full{{$errors->account->has('name') ? ' border-red-400' : ''}}" type="text" name="name" value="{{ old('name', $user->name) }}" />
                         </div>
                         <div class="py-2">
-                            <label for="email" class="block font-medium text-sm text-gray-700{{$errors->account->has('email') ? ' text-red-400' : ''}}">{{ __('Email') }}</label>
+                            <label for="email" class="block text-sm font-medium text-gray-700">{{ __('Email') }}</label>
                             <input id="email" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full{{$errors->account->has('email') ? ' border-red-400' : ''}}" type="email" name="email" value="{{ old('email', $user->email) }}" />
                         </div>
                         <div class="flex justify-end mt-4">
-                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                            <button type="submit" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25">
                                 {{ __('Update') }}
                             </button>
                         </div>
@@ -44,40 +44,43 @@
         </div>
     </div>
     <div class="py-3">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="px-6">
-                    <h1 class="inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 py-4 block sm:inline-block flex">{{ __('Change Password') }}</h1>
+                    <h1 class="flex block inline-block py-4 text-2xl font-extrabold tracking-tight sm:text-3xl text-slate-900 dark:text-slate-200 sm:inline-block">{{ __('Change Password') }}</h1>
                     @if ($errors->password->any())
-                    <ul class="mt-3 list-none list-inside text-sm text-red-400">
+                    <ul class="mt-3 text-sm text-red-400 list-none list-inside">
                         @foreach ($errors->password->all() as $error)
                         <li>{{ $error }}</li>
                         @endforeach
                     </ul>
                     @endif
                     @if(session()->has('password_message'))
-                    <div class="mb-8 text-green-400 font-bold">
+                    <div class="mb-8 font-bold text-green-400">
                         {{ session()->get('password_message') }}
                     </div>
                     @endif
                 </div>
-                <div class="w-full px-6 py-4 bg-white overflow-hidden">
+                <div class="w-full px-6 py-4 overflow-hidden bg-white">
                     <form method="POST" action="{{ route('admin.profile.password.store') }}">
                         @csrf
                         <div class="py-2">
-                            <label for="old_password" class="block font-medium text-sm text-gray-700{{$errors->password->has('old_password') ? ' text-red-400' : ''}}">{{ __('Old Password') }}</label>
-                            <input id="old_password" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full{{$errors->password->has('old_password') ? ' border-red-400' : ''}}" type="password" name="old_password" />
+                            <label for="old_password" class="block text-sm font-medium text-gray-700">{{ __('Old Password') }}</label>
+                            <input id="old_password" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" type="password" name="old_password" />
+                            @error('password')
+								<p class="text-red-500">{{ $message }}</p>
+							@enderror
                         </div>
                         <div class="py-2">
-                            <label for="new_password" class="block font-medium text-sm text-gray-700{{$errors->password->has('new_password') ? ' text-red-400' : ''}}">{{ __('New Password') }}</label>
-                            <input id="new_password" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full{{$errors->password->has('new_password') ? ' border-red-400' : ''}}" type="password" name="new_password" />
+                            <label for="new_password" class="block text-sm font-medium text-gray-700">{{ __('New Password') }}</label>
+                            <input id="new_password" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" type="password" name="new_password" />
                         </div>
                         <div class="py-2">
-                            <label for="confirm_password" class="block font-medium text-sm text-gray-700{{$errors->password->has('confirm_password') ? ' text-red-400' : ''}}">{{ __('Confirm password') }}</label>
-                            <input id="confirm_password" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full{{$errors->password->has('confirm_password') ? ' border-red-400' : ''}}" type="password" name="confirm_password" />
+                            <label for="confirm_password" class="block text-sm font-medium text-gray-700">{{ __('Confirm password') }}</label>
+                            <input id="confirm_password" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" type="password" name="confirm_password" />
                         </div>
                         <div class="flex justify-end mt-4">
-                            <button type='submit' class='inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150'>
+                            <button type='submit' class='inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25'>
                                 {{ __('Change Password') }}
                             </button>
                         </div>
